4cb60b28885c80e436112817376e3ad9d1eec238
ongaeshi <ongaeshi0621@gmail.com>
2011-03-15 02:55:06 +0900

    add base

---
+++ start.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require 'sinatra'
+require 'model/comment.rb'
+
+helpers do
+  include Rack::Utils; alias_method :h, :escape_html
+end
+
+get '/style.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :style
+end
+
+get '/' do
+  @comments = Comments.order_by(:posted_date.desc)
+  haml :index
+end
+
+put '/comment' do
+  Comments.create({
+    :name => request[:name],
+    :title => request[:title],
+    :message => request[:message],
+    :posted_date => Time.now,
+  })
+  redirect '/'
+end
+
+get '/delete' do
+  p params
+  Comments.filter(:id => params[:id]).delete
+  redirect '/'
+end
+
