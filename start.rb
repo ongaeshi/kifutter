ec4f6c39d048bc0c4eb53457c5e2fe40d7a5e94a
ongaeshi <ongaeshi0621@gmail.com>
2011-03-25 02:10:37 +0900

    不要なコードを削除

--- start.rb
+++ start.rb
@@ -8,11 +8,6 @@ helpers do
   include Rack::Utils; alias_method :h, :escape_html
 end
 
-get '/kifutter.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  sass :style
-end
-
 get '/' do
   @total_price = Tweet.sum_price
   @total_count = Tweet.count
