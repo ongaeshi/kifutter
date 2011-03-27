3dc3a92c01ba0fa3bf4f1f49623813f915e5b538
ongaeshi <ongaeshi0621@gmail.com>
2011-03-28 05:20:21 +0900

    cssファイルをscssにすることで、相対パスの依存が無くなるはず

--- start.rb
+++ start.rb
@@ -12,17 +12,11 @@ helpers do
   alias_method :h, :escape_html
 end
 
-get Kifutter::url_root('/*.css') do
-  scss params[:splat][0].to_sym
+get '/*.css' do
+  scss params[:splat].first.to_sym
 end
 
-# hoge/ のような '/' 終わりを '/' 無しへリダイレクト
-# ただし、 http://example.com の場合は末尾 '/' が付く
-get %r{^(.+)/$} do |c|
-  redirect c, 303
-end
-
-get Kifutter::url_root do
+get '/' do
   @total_price = Tweet.sum_price
   @total_count = Tweet.count
   @user_count = User.count
